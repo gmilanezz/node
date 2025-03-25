7e7f3b64ebc6fcc62bb929f1c3f85731243f22f0
Gabriel Milanez <159274083+gmilanezz@users.noreply.github.com>
2025-03-25 10:14:25 -0300

    atualização01 server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const port = 3000;
 const experimentoPath = path.join(__dirname, 'experimento.json');
 
 app.use(express.json());
-app.use(express.urlencoded ({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // Lendo os dados do arquivo JSON
-let experimentoData = fs.readFileSync (experimentoPath, 'utf-8');
+let experimentoData = fs.readFileSync(experimentoPath, 'utf-8');
 let experimento = JSON.parse(experimentoData);
 
 // Função para salvar os dados atualizados no arquivo 350N
@@ -26,28 +26,28 @@ app.get('/adicionarexperimento', (req, res) => {
 
 // Rota para processar a requisição POST do formulário
 app.post('/adicionarexperimento', (req, res) => {
-const novoexperimento = req.body;
-    
-// Verificando se o experimento já existe pelo nome
-if (experimento.find(experimento => experimento.nome.toLowerCase() === novoexperimento.nome.toLowerCase())) {
-    res.send('<h1>experimento já existe. Não é possível adicionar duplicatas.</h1>');
-    return;
-}
+    const novoexperimento = req.body;
 
-app.get('/experimento', (req, res) => {
-    res.json(experimento);
-});
-    
-// Adicionando o novo experimento ao array de experimento
-experimento.push(novoexperimento);
-    
-// Salvando os dados atualizados no arquivo JSON
-salvarDados();
+    // Verificando se o experimento já existe pelo nome
+    if (experimento.find(experimento => experimento.nome.toLowerCase() === novoexperimento.nome.toLowerCase())) {
+        res.send('<h1>experimento já existe. Não é possível adicionar duplicatas.</h1>');
+        return;
+    }
+
+    // Adicionando o novo experimento ao array de experimento
+    experimento.push(novoexperimento);
+
+    // Salvando os dados atualizados no arquivo JSON
+    salvarDados();
     // Enviando uma resposta indicando que o experimento foi adicionado com sucesso
     res.send('<h1>experimento adicionado com sucesso!</h1>');
 });
-    
-     // Iniciar o servidor e escutar na porta especificada
+
+app.get('/experimento', (req, res) => {
+    res.sendFile(path.join(__dirname, 'experimento.json'))
+});
+
+// Iniciar o servidor e escutar na porta especificada
 app.listen(port, () => {
     console.log(`Servidor iniciado em http://localhost:${port}`);
 });
