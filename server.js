eee8a32380a310d0eba82ae279efcb1c1b949dba
gmilanezz <159274083+gmilanezz@users.noreply.github.com>
2025-03-24 21:09:19 -0300

    ac02 sobre node
    Fazer um Banco de Dados científico onde os cientistas podem inserir, filtrar e visualizar os experimentos realizados.

--- server.js
+++ server.js
@@ -1,33 +1,53 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
 
+const app = express();
+const port = 3000;
 
-function readFile(response,file){
-    fs.readFile(file, function(err,data){
-        responde.end(data)
-    })
+const experimentoPath = path.join(__dirname, 'experimento.json');
+
+app.use(express.json());
+app.use(express.urlencoded ({extended: true }));
+
+// Lendo os dados do arquivo JSON
+let experimentoData = fs.readFileSync (experimentoPath, 'utf-8');
+let experimento = JSON.parse(experimentoData);
+
+// Função para salvar os dados atualizados no arquivo 350N
+function salvarDados() {
+    fs.writeFileSync(experimentoPath, JSON.stringify(experimento, null, 2));
 }
 
-function callback(request, response) {
-    response.writeHead(200, {'Content Type': 'application/json; charset=utf-8'})
+// Rota para exibir o formulário HTML
+app.get('/adicionarexperimento', (req, res) => {
+    res.sendFile(path.join(__dirname, 'adicionarexperimento.html'));
+});
 
-    var parts = url.parse(request.url);
-    var path = parts.path
-
-    if(path == 'loja/produtos'){
-        readFile(response, 'loja_produtos.json')
-    } else if (path == 'loja/pedidos') {
-        readFile(response, 'loja_pedidos.json')
-    } else if (path == 'loja/clientes') {
-        readFile(response, 'loja_clientes.json')
-    } else if (path == 'loja/categorias') {
-        readFile(response, 'loja_categorias.json')
-    } else {
-        response.end('Caminho não encontrado: ' + path)
-    }
-
-    var server = http.createServer(callback);
-    server.listen(3000);
-    console.log('Servidor iniciado em http://localhost:3000/');
+// Rota para processar a requisição POST do formulário
+app.post('/adicionarexperimento', (req, res) => {
+const novoexperimento = req.body;
+    
+// Verificando se o experimento já existe pelo nome
+if (experimento.find(experimento => experimento.nome.toLowerCase() === novoexperimento.nome.toLowerCase())) {
+    res.send('<h1>experimento já existe. Não é possível adicionar duplicatas.</h1>');
+    return;
 }
+
+app.get('/experimento', (req, res) => {
+    res.json(experimento);
+});
+    
+// Adicionando o novo experimento ao array de experimento
+experimento.push(novoexperimento);
+    
+// Salvando os dados atualizados no arquivo JSON
+salvarDados();
+    // Enviando uma resposta indicando que o experimento foi adicionado com sucesso
+    res.send('<h1>experimento adicionado com sucesso!</h1>');
+});
+    
+     // Iniciar o servidor e escutar na porta especificada
+app.listen(port, () => {
+    console.log(`Servidor iniciado em http://localhost:${port}`);
+});
